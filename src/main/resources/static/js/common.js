14e180d74a36adf553f974a76f5348568f75c9e6
kanuri jagan satya sai <jagansatyasaikanuri@gmail.com>
2026-05-09 20:46:43 +0530

    Update API base URL to production endpoint

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://service-request-workflow-system-1.onrender.com/api';
 
 const Auth = {
     getToken: () => localStorage.getItem('token'),
