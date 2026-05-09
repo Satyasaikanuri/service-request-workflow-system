89894d11f173452594959e85cfe1e8a33f2a804e
kanuri jagan satya sai <jagansatyasaikanuri@gmail.com>
2026-05-09 19:12:51 +0530

    Add findByUsername method to UserRepository

--- src/main/java/com/softwarecompany/serviceportal/repositories/UserRepository.java
+++ src/main/java/com/softwarecompany/serviceportal/repositories/UserRepository.java
@@ -5,18 +5,22 @@ import com.softwarecompany.serviceportal.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 
     Boolean existsByEmail(String email);
 
     Boolean existsByUsername(String username);
 
-    // Count users by their role name
+    
     long countByRole_Name(Role.RoleName roleName);
 
-    java.util.List<User> findByDepartmentId(Long departmentId);
+    List<User> findByDepartmentId(Long departmentId);
 }
