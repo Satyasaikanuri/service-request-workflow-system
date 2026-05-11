da014db8542773d3fc8ef9d17b63eba196346770
kanuri jagan satya sai <jagansatyasaikanuri@gmail.com>
2026-05-11 12:31:30 +0530

    Refactor UserDetailsServiceImpl for clarity

--- src/main/java/com/softwarecompany/serviceportal/security/UserDetailsServiceImpl.java
+++ src/main/java/com/softwarecompany/serviceportal/security/UserDetailsServiceImpl.java
@@ -13,142 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowiredpackage com.softwarecompany.serviceportal.security;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.softwarecompany.serviceportal.models.User;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-
-public class UserDetailsImpl implements UserDetails {
-
-    private static final long serialVersionUID = 1L;
-
-    private Long id;
-
-    private String username;
-
-    private String email;
-
-    @JsonIgnore
-    private String password;
-
-    private Collection<? extends GrantedAuthority> authorities;
-
-    private Long departmentId;
-
-    public UserDetailsImpl(
-            Long id,
-            String username,
-            String email,
-            String password,
-            Collection<? extends GrantedAuthority> authorities,
-            Long departmentId) {
-
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.authorities = authorities;
-        this.departmentId = departmentId;
-    }
-
-    public static UserDetailsImpl build(User user) {
-
-        List<GrantedAuthority> authorities = List.of(
-                new SimpleGrantedAuthority(
-                        "ROLE_" + user.getRole().getName().name()));
-
-        return new UserDetailsImpl(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getPassword(),
-                authorities,
-                user.getDepartment() != null
-                        ? user.getDepartment().getId()
-                        : null);
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (this == o)
-            return true;
-
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        UserDetailsImpl user = (UserDetailsImpl) o;
-
-        return Objects.equals(id, user.id);
-    }
-}
-    UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
 
-        User user = userRepository.findByUsername(username)
+        User user = userRepository
+                .findByUsername(username)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(
-                                "User Not Found with username: " + username
-                        )
-                );
+                                "User Not Found with username: " + username));
 
         return UserDetailsImpl.build(user);
     }
